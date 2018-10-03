786e8a0e323d435ae7614f98743455c6d1b42b30
Md. Alamin Mahamud <alamin.ineedahelp@gmail.com>
2018-10-04 00:54:36 +0600

    :tada: docker-compose done

---
+++ HISTORY.md
@@ -0,0 +1,3 @@
+#### Features
+1. EVE-0001: Setting up Docker-compose [6h]
+    - plug and play docker containers of different technologies.
--- everything/settings.py
+++
@@ -1,8 +0,0 @@
-import ast
-import os.path
-
-import dj_database_url
-import dj_email_url
-import django_cache_url
-
-from django.contrib.messages import constants as messages
